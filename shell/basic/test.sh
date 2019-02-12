fd43bc48128ec75a88a5eec1daacf56362677447
shi-hao <shi7631470@163.com>
2019-02-12 10:05:52 +0800

    add

--- shell/basic/test.sh
+++ shell/basic/test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#====================================================
+# Author: shi
+# Create Date: 2019-02-11
+# Description: 
+#====================================================
 
+###############################################
 #定义变量 var=value  取变量值 $var  
 x=10 y=20
 dollar=$
@@ -8,10 +14,16 @@ dollar=$
 s=$x$y'$'
 ss=$x'1234'
 
+echo $s
+echo $ss
+echo $dollar
+
+##################################################
 #eval command argument
 eval ls -l
 
 
+####################################################
 #if
 if 1 ; then
 echo $x
@@ -19,9 +31,31 @@ else
 echo $y
 fi
 
+#check the dir in home path, if not exist, create it
+if [ ! -d $HOME"/mmtest" ];then
+	echo $HOME"/mmtest not exist"
+	mkdir $HOME/mmtest
+else
+	echo $HOME"/mmtest exist"
+fi
+
+cp   ./*  $HOME/mmtest
+cat  ./test.sh >> $HOME/mmtest/test.sh
+
+#grep
+string='boy2'
+filename='1.txt'
+if grep -q $string $filename  ;then 
+   echo "OK";
+else
+   echo "NOT OK";
+fi
+
+##################################################
 #for
 
 
-echo $s
-echo $ss
-echo $dollar
+#########################################
+#timestampe
+timestamp=$(date +%s)
+echo $timestamp
---
+++ shell/sed/sed_test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#====================================================
+# Author: shi
+# Create Date: 2019-02-12
+# Description: 
+#====================================================
+
+# Let’s take an plain text file, input.txt, that looks like this
+# 
+# PATTERN-1
+# First line of unimportant text
+# Second line of unimportant text
+# PATTERN-2
+# Some more texts (may/ mayn't be important!)
+# 
+# We want to delete some of the lines from the file using the command line stream editor, sed.
+# 
+# 1. Use the following command to delete the lines lying between PATTERN-1 and PATTERN-2 , excluding the lines 
+# containing these patterns:
+# 
+# sed '/PATTERN-1/,/PATTERN-2/{//!d}' input.txt
+# 
+# If you want to modify the file itself, instead of just the file stream, include the “-i” flag after sed.
+# 
+# 2. Use the following command to delete the lines lying between PATTERN-1 and PATTERN-2 , including the lines
+# containing these patterns:
+# 
+# sed '/PATTERN-1/,/PATTERN-2/d' input.txt
+# 
+# 3. To delete all the lines after PATTERN-2, use this
+# 
+# sed '/PATTERN-1/,$d' input.txt
+# 
+# 4. To delete lines, say 2 through 4 (if you know the correct line numbers, of course!), use this
+# 
+# sed '2,4d' input.txt
+# 
+# Here is a good sed reference.
+
+sed '/#start/,/#end/d' 1.txt > 2.txt
