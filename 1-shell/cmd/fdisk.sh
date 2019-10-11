016583141425a03cb22c339fb0b467a5b3863fd0
bleach <shi7631470@163.com>
2019-10-11 14:09:53 +0800

    Dedit

--- 1-shell/cmd/fdisk.sh
+++ 1-shell/cmd/fdisk.sh
@@ -1,22 +1,24 @@
 #!/bin/bash 
 
 #查看磁盘及分区
-fdisk 
+#可以查看系统所有的磁盘和分区
+sudo fdisk -l
 
 
-#删除分区
+#删除分区，新建分区，根据提示选择指令，注意最后一定要输入w保存退出，否则更改不会生效
+sudo fdisk  <disk_name>
 
-
-#新建分区
-
+d  delete partition
+w  save and exit
 
 #格式化磁盘
 
 
 #挂载磁盘
-mount
-umount
+#磁盘接入系统后，将其挂载到文件系统的某个路径下，才能使用
+mount   <disk_partition>   <file_dir>
+umount  <disk_partition>
 
 
-#查看磁盘挂载位置
+#查看磁盘挂载到文件系统中的位置（路径）
 df
