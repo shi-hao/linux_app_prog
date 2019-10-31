6d5ecb8a9ffbb1b4c293e4d4afa0948391fc7240
shi-hao <shi7631470@163.com>
2019-10-31 12:58:29 +0800

    edit

--- 1-shell/script/readCfg.sh
+++ 1-shell/script/readCfg.sh
@@ -2,12 +2,24 @@
 
 while read line
 do  
-	eval "$line"  
+eval "$line"  
 done < 2.txt  
 
 echo $ip
 echo $name
 
+
+
+while true
+do
+	isContinue='y'
+	read -p 'continue? [y/n]' isContinue
+	echo $isContinue
+	
+	if [ $isContinue == 'y' ]; then
+		break
+	fi
+done
 
 source 2.txt
 
